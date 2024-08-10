e1da095c7a5b97ac7677ade8cd72bc4e78914d6c
romaniyazov <romaniyazov@gmail.com>
2024-08-10 18:42:39 +0300

    Sprint3 Counter App

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainCountLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    let dateFormatter = DateFormatter()
+    
+    var count: Int = 0 {
+        didSet {
+            mainCountLabel.text = "\(count)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        count = 0
+        historyTextView.text += "\n\(currentDatePretty()): значение сброшено"
     }
 
+    @IBAction func minusButtonPressed(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            historyTextView.text += "\n\(currentDatePretty()): значение изменено на -1"
+        } else {
+            historyTextView.text += "\n\(currentDatePretty()): попытка уменьшить значение счётчика ниже 0"
+        }
+    }
 
+    @IBAction func plusButtonPressed(_ sender: Any) {
+        count += 1
+        historyTextView.text += "\n\(currentDatePretty()): значение изменено на +1"
+    }
+    
+    func currentDatePretty() -> String {
+        dateFormatter.string(from: Date())
+    }
 }
-
